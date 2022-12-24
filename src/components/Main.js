29fa172d03ee90422a5000f4a59e89183b03c454
Alexey Belkov <lexabelco2013@yandex.ru>
2022-12-25 00:45:09 +0500

    feat: настроить работу 3 попапов

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,23 @@
 import avatarLoaderGif from "../images/avatar-loader.gif";
 
 function Main() {
+  const handleEditAvatarClick = () => {
+    document.querySelector("#popup_avatar").classList.add("popup_opened");
+  };
+
+  const handleEditProfileClick = () => {
+    document.querySelector("#popup_profile").classList.add("popup_opened");
+  };
+
+  const handleAddPlaceClick = () => {
+    document.querySelector("#popup_gallery").classList.add("popup_opened");
+  };
+
   return (
     <main>
       <section className="profile">
         <div className="profile__info">
-          <button className="profile__picture-btn" type="button">
+          <button className="profile__picture-btn" type="button" onClick={handleEditAvatarClick}>
             <img
               className="profile__picture"
               src={avatarLoaderGif}
@@ -14,11 +26,11 @@ function Main() {
           </button>
           <div className="profile__user">
             <h1 className="profile__user-name">Загрузка...</h1>
-            <button className="profile__edit-btn" type="button"></button>
+            <button className="profile__edit-btn" type="button" onClick={handleEditProfileClick}></button>
             <p className="profile__user-about">Загрузка...</p>
           </div>
         </div>
-        <button className="profile__add-btn" type="button"></button>
+        <button className="profile__add-btn" type="button" onClick={handleAddPlaceClick}></button>
       </section>
 
       <section className="gallery">
