7ce66715e04c1a9fb1f86dc21c87a45af9596566
Alexey Belkov <lexabelco2013@yandex.ru>
2022-12-25 02:07:45 +0500

    feat: подключить api для данных о пользователе

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,24 @@
+import React from "react";
 import avatarLoaderGif from "../images/avatar-loader.gif";
+import api from "../utils/Api"
 
 function Main(props) {
+  const [userName, setUserName] = React.useState('Загрузка...');
+  const [userDescription, setUserDescription] = React.useState('Загрузка...');
+  const [userAvatar, setUserAvatar] = React.useState(avatarLoaderGif);
+
+  React.useEffect(() => {
+    api.getInitialUserInfo()
+    .then((result) => {
+      setUserName(result.name);
+      setUserDescription(result.about);
+      setUserAvatar(result.avatar);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  }, []);
+
   return (
     <main>
       <section className="profile">
@@ -8,14 +26,14 @@ function Main(props) {
           <button className="profile__picture-btn" type="button" onClick={props.onEditAvatar}>
             <img
               className="profile__picture"
-              src={avatarLoaderGif}
+              src={userAvatar}
               alt="аватарка пользователя"
             />
           </button>
           <div className="profile__user">
-            <h1 className="profile__user-name">Загрузка...</h1>
+            <h1 className="profile__user-name">{userName}</h1>
             <button className="profile__edit-btn" type="button" onClick={props.onEditProfile}></button>
-            <p className="profile__user-about">Загрузка...</p>
+            <p className="profile__user-about">{userDescription}</p>
           </div>
         </div>
         <button className="profile__add-btn" type="button" onClick={props.onAddPlace}></button>
